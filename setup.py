f96cac11efe0b06caf80059927c624c19c5e91e2
Jongwon Han <addnull@gmail.com>
2014-09-16 19:57:00 +0900

    hot fix: remove install requirement 'swift3 >= 1.7.0'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='swift3_gatekeeper',
       author='a2company',
       author_email='admin@a2company.co.kr',
       packages=['swift3_gatekeeper'],
-      install_requires=['swift >= 1.13.0', 'swift3 >= 1.7.0'],
+      install_requires=['swift >= 1.13.0'],
       entry_points={'paste.filter_factory':
                         ['swift3_gatekeeper='
                          'swift3_gatekeeper.middleware:filter_factory']})
