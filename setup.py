dcbc8eedf0acd1aeca14fd7aa0472097c4d7708d
metocks <metocks@a2company.com>
2015-04-06 17:36:21 +0900

    ST-480: Unify different python packaging methods into use cfg file

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python
+# Copyright (c) 2015 A2Company.
+#
+
 from setuptools import setup
 
-import swift3_gatekeeper
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-setup(name='swift3_gatekeeper',
-      version=swift3_gatekeeper.version,
-      description='Middleware to filter S3 relational user-metadata in swift',
-      license='Apache License (2.0)',
-      author='a2company',
-      author_email='admin@a2company.co.kr',
-      packages=['swift3_gatekeeper'],
-      install_requires=['swift >= 1.13.0'],
-      entry_points={'paste.filter_factory':
-                        ['swift3_gatekeeper='
-                         'swift3_gatekeeper.middleware:filter_factory']})
+setup(
+    setup_requires=['pbr'],
+    pbr=True
+) 
