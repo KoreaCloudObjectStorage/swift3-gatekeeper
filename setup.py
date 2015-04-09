aab792744ccd9c5dfd431291988108f35ddb94c8
Siheon Kim <metocks@a2company.co.kr>
2015-04-09 14:23:46 +0900

    remove deprecated code

--- setup.py
+++ setup.py
@@ -4,14 +4,6 @@
 
 from setuptools import setup
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
-
 setup(
     setup_requires=['pbr'],
     pbr=True
